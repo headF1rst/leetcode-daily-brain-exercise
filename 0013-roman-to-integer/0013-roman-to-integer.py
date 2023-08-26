54ee851e2384c5348d5e173665f1fcd55b333f73
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2023-08-26 12:04:02 +0900

    Time: 54 ms (69.67%), Space: 16.2 MB (97.5%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,11 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         n = len(s)
-        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         answer = 0
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
         for i in range(n):
-            if i < n - 1 and dic[s[i]] < dic[s[i + 1]]:
+            if i < n - 1 and dic[s[i + 1]] > dic[s[i]]:
                 answer -= dic[s[i]]
             else:
                 answer += dic[s[i]]
